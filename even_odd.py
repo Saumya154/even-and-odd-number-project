eb1dad17d312037118b1dbc8288adb3d8ad69887
Saumya154 <saumyagupta2420@gmail.com>
2021-05-15 20:52:53 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+def parent(n): //main function
+    def even(n):
+        if(n%2==0):
+            print("even")
+# Shashank odd func goes here
+
+
+if __name__=="__main__":
+    n=int(input())
+    parent(n)
